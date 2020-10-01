c6bedbcc1e30429cbba18b6f2b3e6404bc5a5d12
jplozada <juxnpx@gmail.com>
2020-09-30 23:26:03 -0400

    fetch API data from NewsApi.org

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React, {Fragment, useState} from 'react';
+import React, {Fragment, useState, useEffect} from 'react';
 import Header from './components/Header';
 import Formulario from './components/Formulario';
 
 function App() {
 
   // Definir la categoria y noticias
-  const [categoria, guardarCategoria] = useState('')
+  const [categoria, guardarCategoria] = useState('');
+  const [noticias, guardarNoticias] = useState([]);
+
+  useEffect(() => {
+    const consultarAPI = async () => {
+      const url = `http://newsapi.org/v2/top-headlines?country=mx&category=${categoria}&apiKey=adeeb2b98624494fa3859f5f6dae8edb`;
+      const respuesta = await fetch(url);
+      const noticias = await respuesta.json();
+
+      guardarNoticias(noticias.articles);
+    }
+    consultarAPI();
+  }, [categoria])
 
   return (
     <Fragment>
